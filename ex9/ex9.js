eeb71910d0181f30ed777d91cb563b86f38c1648
showthesunli <showthesunli@gmail.com>
2022-11-17 12:37:29 +0800

    accomplished ex9

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -1,9 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	var $btn = $("#btn"),
 		$list = $("#list");
 
-	$btn.click(/* TODO */);
+	var clickChan = ASQ.csp.chan();
+	var messageChan = ASQ.csp.chan();
 
+	$btn.click(clickListener);
+
+	function clickListener(evn) {
+		ASQ.csp.putAsync(clickChan, evn)
+	}
+
+	function* clickConsumer() {
+		while (true) {
+			var evn = yield ASQ.csp.take(clickChan)
+			console.log('evn :>> ', evn);
+			yield ASQ.csp.put(messageChan, "click")
+		}
+	}
+
+	function* messageConsumer() {
+		while (true) {
+			yield ASQ.csp.take(ASQ.csp.timeout(1000))
+			var message = yield ASQ.csp.take(messageChan)
+			$list.append($("<div>" + message + "</div>"));
+			console.log('message :>> ', message);
+		}
+	}
+
+	ASQ().runner(
+		ASQ.csp.go(clickConsumer),
+		ASQ.csp.go(messageConsumer)
+	)
 	// Hint: ASQ().runner( .. )
 
 	// TODO: setup sampling go-routine and
